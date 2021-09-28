a192d72969893f79638429433a1059b3135b868f
Sylpha-Program <sylpha.program@gmail.com>
2021-09-28 19:00:37 +0900

    update pokemons controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -38,6 +38,10 @@ class PokemonsController < ApplicationController
 
   def random
     rand = Rails.env.production? ? "RANDOM()" : "rand()"
+    @score = Score.find_by(user_id: 1, pokemon_id: 8, stage_id: 4)
+    @score.total_point = 151
+    @score.max_point = 50
+    @score.save
     @pokemon = Pokemon.order(rand).first
     session[:pokemon_id] = @pokemon.id
     redirect_to root_url
@@ -51,7 +55,7 @@ class PokemonsController < ApplicationController
   end
 
   def score_update
-    @score = Score.find_by(user_id: session[:user_id], pokemon_id: params[:id], stage: params[:stage_id])
+    @score = Score.find_by(user_id: session[:user_id], pokemon_id: params[:id], stage_id: params[:stage_id])
     @previous_level = Math.sqrt((Score.where(user_id: session[:user_id], pokemon_id: params[:id]).sum(:total_point)) / 10).floor
     @score.total_point += params[:point].to_i
     @score.save
